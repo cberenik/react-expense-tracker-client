12067b65adfdc6a4b80d52162b1261c96ebc1047
Conrad Berenik <conrad.berenik@gmail.com>
2018-11-10 11:22:25 -0600

    Figure out how to display mini logo image on local

--- src/page-components/PageShell.jsx
+++ src/page-components/PageShell.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Menu, Container, Image } from 'semantic-ui-react';
+import dollarSign from '../dollar-sign.png';
 
 export function PageShell(props) {
     return (
         <div>
             <Menu fixed="top" inverted>
                 <Menu.Item header>
-                    <Image size="mini" src="../dollar-sign.png" style={{ marginRight: '1.5em' }} />
+                    <Image size="mini" src={dollarSign} style={{ marginRight: '1.5em' }} />
                     Expense Tracker
                 </Menu.Item>
             </Menu>
